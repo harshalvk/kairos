16a0e108fd03dbbde7c30f15552a37d4f43f958f
Harshalvk <harshalvkhobragade@gmail.com>
2026-08-11 18:03:37 +0530

    fix: add security comment to ShardFor method to clarify shardCount validation

--- internal/shard/shard.go
+++ internal/shard/shard.go
@@ -29,7 +29,7 @@ func (r *Router) ShardFor(jobType string) int {
 	}
 	h := fnv.New32a()
 	_, _ = h.Write([]byte(jobType)) // hash.Hash.Write never returns an error
-	return int(h.Sum32() % uint32(r.shardCount))
+	return int(h.Sum32() % uint32(r.shardCount)) // #nosec G115 -- shardCount is validated to fit within uint32
 }
 
 // ShardCount returns the total number of shards
